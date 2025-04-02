ff20690f739fb112c7d05e996348bb7c6fce646d
nebesnayaa <s.nebesnaya5@gmail.com>
2025-04-02 21:20:50 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+} else {
+  console.error("Root element not found");
+}
+
