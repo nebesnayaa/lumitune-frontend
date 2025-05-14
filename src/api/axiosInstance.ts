4cc65f46373731f38fb77067d6ce0ff5b574ad33
nebesnayaa <s.nebesnaya5@gmail.com>
2025-05-14 16:08:10 +0300

    server address fix

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://lumituneapp.azurewebsites.net/", // додай /api якщо в тебе API лежить під цим шляхом
+  baseURL: "https://lumituneapp.azurewebsites.net/", // додай /api якщо в тебе API лежить під цим шляхом
   headers: {
     "Content-Type": "application/json",
   },
